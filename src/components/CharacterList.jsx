bca6a9fd7667ad855966369da8774d86d13704ec
SridharshiniArumugam <sridharshiniece@gmail.com>
2026-05-05 10:05:42 +0530

    fix: changes related to api file

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -7,7 +7,7 @@
 
 import React, { useEffect, useState } from "react";
 import Character from "./Character";
-import { fetchCharacters } from "../services/api";
+import { fetchCharacters } from "../services/Api.js";
 import "./CharacterList.scss";
 
 const CharacterList = () => {
--- src/services/Api.js
+++ src/services/Api.js
@@ -2,17 +2,6 @@ import axios from "axios";
 
 const BASE_URL = "https://rickandmortyapi.com/api";
 
-// export const fetchCharacters = async (page, name, status) => {
-//   try {
-//     const res = await axios.get(`${BASE_URL}/character`, {
-//       params: { page, name, status },
-//     });
-//     return res.data;
-//   } catch {
-//     return { results: [], info: { pages: 0 } };
-//   }
-// };
-
 export const fetchCharacters = async (page,name,status) => {
     try {
         const res = await axios.get(`https://rickandmortyapi.com/api/character`, {
